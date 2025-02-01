286fb5e6305f08675e52b22f126a30db95627857
TranNgocChi <brightsuntnc2003@gmail.com>
2025-02-01 21:35:54 +0700

    Fix build error

--- src/GOCAP.Repository/GroupRepository.cs
+++ src/GOCAP.Repository/GroupRepository.cs
@@ -5,8 +5,9 @@ namespace GOCAP.Repository;
 internal class GroupRepository(AppSqlDbContext context, IMapper mapper) : SqlRepositoryBase<Group, GroupEntity>(context, mapper), IGroupRepository
 {
     private readonly AppSqlDbContext _context = context;
-    public async Task<List<Group>> GetByUserIdWithPagingAsync(QueryInfo queryInfo, Guid userId)
+    public Task<List<Group>> GetByUserIdWithPagingAsync(QueryInfo queryInfo, Guid userId)
     {
         var groupQuery = _context.Groups.AsQueryable();
+        return Task.FromResult(new List<Group>());
     }
 }
