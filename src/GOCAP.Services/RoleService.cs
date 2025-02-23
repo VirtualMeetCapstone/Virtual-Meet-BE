dcc5f3ba3a32072fa63c62d27760c4418f75dd06
TranNgocChi <brightsuntnc2003@gmail.com>
2025-02-23 23:36:30 +0700

    feat: Assign role to user

---
+++ src/GOCAP.Services/RoleService.cs
@@ -0,0 +1,9 @@
+﻿namespace GOCAP.Services;
+
+[RegisterService(typeof(IRoleService))]
+internal class RoleService (
+    IRoleRepository _repository,
+    IMapper _mapper,
+    ILogger<RoleService> _logger) : ServiceBase<Role, RoleEntity>(_repository, _mapper, _logger), IRoleService
+{
+}
