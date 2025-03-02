1f7a33862f99d2c3671e212aa414f0fe89151427
TranNgocChi <brightsuntnc2003@gmail.com>
2025-03-02 23:09:29 +0700

    refactor: Modify story highlight

--- src/GOCAP.Api/Controllers/StoriesController.cs
+++ src/GOCAP.Api/Controllers/StoriesController.cs
@@ -1,11 +1,10 @@
 ﻿namespace GOCAP.Api.Controllers;
 
-[Route("stories")]
 public class StoriesController(
     IStoryService _service,
     IStoryReactionService _storyReactionService,
     IStoryViewService _storyViewService,
-    IStoryHightLightService _storyHightLightService,
+    IStoryHighlightService _storyHightLightService,
     IMapper _mapper) : ApiControllerBase
 {
     /// <summary>
@@ -13,45 +12,25 @@ public class StoriesController(
     /// </summary>
     /// <param name="model"></param>
     /// <returns>story model</returns>
-    [HttpPost]
+    [HttpPost("stories")]
     [ValidateModel]
     public async Task<StoryModel> CreateNewStory([FromForm] StoryCreationModel model)
     {
         var domain = _mapper.Map<Story>(model);
         var result = await _service.AddAsync(domain);
         return _mapper.Map<StoryModel>(result);
-    }
-
-    /// <summary>
-    /// Get the stories which user is following.
-    /// </summary>
-    /// <param name="userId"></param>
-    /// <param name="queryInfo"></param>
-    /// <returns>QueryResult<StoryModel></returns>
-    [HttpGet("{userId}/following")]
-    public async Task<QueryResult<StoryModel>> GetFollowingStoriesWithPaging([FromRoute] Guid userId, [FromQuery] QueryInfo queryInfo)
-    {
-        var stories = await _service.GetFollowingStoriesWithPagingAsync(userId, queryInfo);
-        return _mapper.Map<QueryResult<StoryModel>>(stories);
-    }
+    } 
 
     /// <summary>
     /// Get story detail by id.
     /// </summary>
     /// <param name="id"></param>
     /// <returns>StoryDetailModel</returns>
-    [HttpGet("{id}")]
+    [HttpGet("stories/{id}")]
     public async Task<StoryDetailModel> GetStoryById([FromRoute] Guid id)
     {
         var domain = await _service.GetByIdAsync(id);
         return _mapper.Map<StoryDetailModel>(domain);
-    }
-
-    [HttpGet("users/{userId}")]
-    public async Task<QueryResult<StoryUserModel>> GetStoriesByUserId([FromRoute] Guid userId, [FromQuery] QueryInfo queryInfo)
-    {
-        var domain = await _service.GetStoriesByUserIdWithPagingAsync(userId, queryInfo);
-        return _mapper.Map<QueryResult<StoryUserModel>>(domain);
     }
 
     /// <summary>
@@ -59,11 +38,21 @@ public class StoriesController(
     /// </summary>
     /// <param name="id"></param>
     /// <returns>OperationResult</returns>
-    [HttpDelete("{id}")]
+    [HttpDelete("stories/{id}")]
     public async Task<OperationResult> DeleteStory([FromRoute] Guid id)
     {
         return await _service.DeleteByIdAsync(id);
     }
+
+    /// <summary>
+    /// Remove hight light story.
+    /// </summary>
+    /// <param name="storyId"></param>
+    /// <param name="storyHighlightId"></param>
+    /// <returns>Operation result</returns>
+    [HttpDelete("stories/{storyId}/highlight/{storyHighlightId}")]
+    public async Task<OperationResult> RemoveHightlightStory([FromRoute] Guid storyId, [FromRoute] Guid storyHighlightId)
+    => await _storyHightLightService.DeleteAsync(storyId, storyHighlightId);
 
     /// <summary>
     /// Create or delete story reaction.
@@ -71,7 +60,7 @@ public class StoriesController(
     /// <param name="storyId"></param>
     /// <param name="model"></param>
     /// <returns>OperationResult</returns>
-    [HttpPost("{storyId}/reactions")]
+    [HttpPost("stories/{storyId}/reactions")]
     public async Task<OperationResult> CreateOrDeleteReaction([FromRoute] Guid storyId, [FromBody] StoryReactionCreationModel model)
     {
         var domain = _mapper.Map<StoryReaction>(model);
@@ -86,7 +75,7 @@ public class StoriesController(
     /// <param name="storyId"></param>
     /// <param name="queryInfo"></param>
     /// <returns>StoryReactionDetailModel</returns>
-    [HttpGet("{storyId}/reactions/page")]
+    [HttpGet("stories/{storyId}/reactions")]
     public async Task<QueryResult<StoryReactionDetailModel>> GetReactionDetailsWithPaging([FromRoute] Guid storyId, [FromQuery] QueryInfo queryInfo)
     {
         var domain = await _storyReactionService.GetReactionDetailsWithPagingAsync(storyId, queryInfo);
@@ -100,7 +89,7 @@ public class StoriesController(
     /// <param name="storyId"></param>
     /// <param name="model"></param>
     /// <returns>StoryViewModel</returns>
-    [HttpPost("{storyId}/views")]
+    [HttpPost("stories/{storyId}/views")]
     public async Task<StoryViewModel> RecordStoryView([FromRoute] Guid storyId, [FromBody] StoryViewCreationModel model)
     {
         var domain = _mapper.Map<StoryView>(model);
@@ -115,7 +104,7 @@ public class StoriesController(
     /// <param name="storyId"></param>
     /// <param name="queryInfo"></param>
     /// <returns></returns>
-    [HttpGet("{storyId}/views")]
+    [HttpGet("stories/{storyId}/views")]
     public async Task<QueryResult<StoryViewDetailModel>> GetStoryViewers([FromRoute] Guid storyId, [FromQuery] QueryInfo queryInfo)
     {
         var domain = await _storyViewService.GetWithPagingAsync(storyId, queryInfo);
@@ -124,31 +113,48 @@ public class StoriesController(
     }
 
     /// <summary>
+    /// Get stories by user id.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="queryInfo"></param>
+    /// <returns></returns>
+    [HttpGet("users/{userId}/stories")]
+    public async Task<QueryResult<StoryUserModel>> GetStoriesByUserId([FromRoute] Guid userId, [FromQuery] QueryInfo queryInfo)
+    {
+        var domain = await _service.GetStoriesByUserIdWithPagingAsync(userId, queryInfo);
+        return _mapper.Map<QueryResult<StoryUserModel>>(domain);
+    }
+
+    /// <summary>
+    /// Get the stories which user is following.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="queryInfo"></param>
+    /// <returns>QueryResult<StoryModel></returns>
+    [HttpGet("users/{userId}/followings/stories")]
+    public async Task<QueryResult<StoryModel>> GetFollowingStoriesWithPaging([FromRoute] Guid userId, [FromQuery] QueryInfo queryInfo)
+    {
+        var stories = await _service.GetFollowingStoriesWithPagingAsync(userId, queryInfo);
+        return _mapper.Map<QueryResult<StoryModel>>(stories);
+    }
+
+    /// <summary>
     /// Add one hight light story to user profile.
     /// </summary>
     /// <param name="storyId"></param>
     /// <param name="model"></param>
     /// <returns>StoryHightLightModel</returns>
-    [HttpPost("{storyId}/hightlight")]
-    public async Task<StoryHightLightModel> AddHightlightStory([FromRoute] Guid storyId, [FromBody] StoryHightLightCreationModel model)
+    [HttpPost("users/{userId}/stories/{storyId}/highlight")]
+    public async Task<StoryHighlightModel> AddHighlightStory([FromRoute] Guid userId, [FromRoute] Guid storyId, [FromBody] StoryHighlightCreationModel model)
     {
-        var domain = _mapper.Map<StoryHightLight>(model);
+        var domain = _mapper.Map<StoryHighlight>(model);
         domain.StoryId = storyId;
+        domain.UserId = userId;
         var result = await _storyHightLightService.AddAsync(domain);
-        return _mapper.Map<StoryHightLightModel>(result);
+        return _mapper.Map<StoryHighlightModel>(result);
     }
 
-    /// <summary>
-    /// Remove hight light story.
-    /// </summary>
-    /// <param name="storyId"></param>
-    /// <param name="storyHighlightId"></param>
-    /// <returns>Operation result</returns>
-    [HttpDelete("{storyId}/hightlight/{storyHighlightId}")]
-    public async Task<OperationResult> RemoveHightlightStory([FromRoute] Guid storyId, [FromRoute] Guid storyHighlightId)
-    => await _storyHightLightService.DeleteAsync(storyId, storyHighlightId);
-
-    [HttpGet("highlight/users/{userId}")]
+    [HttpGet("users/{userId}/story-highlights")]
     public async Task<List<List<StoryModel>>> GetStoryHighlightByUserId([FromRoute] Guid userId)
     {
         var result = await _storyHightLightService.GetStoryHighlightByUserIdAsync(userId);
